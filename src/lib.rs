789ec93e04883d77bb5b464adf5241131ea88cbb
xsimple1010 <aderval758@gmail.com>
2024-03-17 17:23:47 -0300

    export GamepadStateListener

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate lazy_static;
 mod handle_event;
 mod thread_event;
 
-pub use handle_event::GamePadState;
+pub use handle_event::{GamePadState, GamepadStateListener};
 pub mod context;
 pub mod key_map;
 pub mod retro_gamepad;
