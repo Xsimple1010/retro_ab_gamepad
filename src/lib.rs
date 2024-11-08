b74c79baa173868b0b94a4fc6103a4b120e1a0a6
xsimple1010 <aderv78@gmail.com>
2024-11-07 21:13:20 -0300

    DeviceStateListener é importado de forma publica

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod gamepad;
 mod retro_ab_controller;
 mod state_thread;
 
-pub use devices_manager::{Device, DeviceState};
+pub use devices_manager::{Device, DeviceState, DeviceStateListener};
 pub use retro_ab_controller::{
     input_poll_callback, input_state_callback, rumble_callback, RetroAbController,
 };
